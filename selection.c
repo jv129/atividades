c3a71fa1c0c046c09ce1403b768d1bed9efc6d63
josé Vitor <78517762+jv129@users.noreply.github.com>
2025-02-04 13:25:46 -0300

    Add files via upload

---
+++ selection.c
@@ -0,0 +1,34 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+	//selection
+	srand(time(NULL));
+	int vet[10];
+	for(int i = 0; i < 10; i++){
+		vet[i] = rand()%15;
+		printf("[%d]",vet[i]);
+	}
+	printf("\n");
+	int aux;
+	for(int i = 9; i > -1; i--){
+		int maior = vet[0], p = 0;
+		for(int j = 0; j < i+1; j++){
+			if(vet[j] > maior){
+				maior = vet[j];
+				p = j;
+			}
+		}
+		aux = vet[p];
+		vet[p] = vet[i];
+		vet[i] = aux;
+	}
+	for(int i = 0; i < 10; i++){
+		printf("[%d]", vet[i]); 
+	}
+	return 0;
+}
+
